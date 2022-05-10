b6e3c10fe2af4a60b3089a7fe156d52c2d35114e
PPF\yamaoka <yamaoka@ppfpart.co.jp>
2022-05-10 16:50:38 +0900

    テーブル補修

--- resources/views/admin/request/admin_request.blade.php
+++ resources/views/admin/request/admin_request.blade.php
@@ -5,8 +5,8 @@
         <h2 class="fw-bold">申請一覧</h2>
     
         <hr>
-        <a href='/admin/attends/new' class="btn btn-primary">検索</a>
-        <a href='/admin/attends/new' class="btn btn-primary">追加</a>
+        <a href='' class="btn btn-primary">検索</a>
+        <a href='' class="btn btn-primary">追加</a>
 
         
         <table class="table">
@@ -26,10 +26,23 @@
             @foreach($all_requests as $request)
             <tr>
                 <td>{{$request -> user -> name}}</td>
-                <td>{{$request -> related_request() -> count() +1}}</td>
+                @if($request -> type == 1)
+                <td>{{$request -> related_request() -> count() +1}}日<br>                
+                    ({{$request -> time ?? "記入なし"}})
+                @else
+                <td>{{$request -> related_request() -> count() +1}}
+                日
+                @endif
+                </td>
                 <td>{{$request -> request_types -> name}}</td>
-                <td>{{$request -> comment ?? "記入なし"}}</td>
+                <td>
+                    <div class="overflow-auto" style = "max-height: 48px; max-width: 360px">
+                        {{$request -> reason ?? "記入なし"}}
+                    </div>
+                </td>
+                
                 <td>    
+                    <a href="#?id={{$request ->id}}" class="btn btn-primary">詳細</a>
                     <a href="#" class="btn btn-primary">承認</a>
                     <a href="#" class="btn btn-secondary">却下</a>
                 </td>
