eeb373586a8e526ac35c4086a45ccabc744b13b4
PPF\yamaoka <yamaoka@ppfpart.co.jp>
2022-05-10 17:01:58 +0900

    モーダルの作成

--- resources/views/admin/request/admin_request.blade.php
+++ resources/views/admin/request/admin_request.blade.php
@@ -36,15 +36,21 @@
                 </td>
                 <td>{{$request -> request_types -> name}}</td>
                 <td>
-                    <div class="overflow-auto" style = "max-height: 48px; max-width: 360px">
+                    <div class="overflow-auto" style = "max-height: 48px; max-width: 260px">
                         {{$request -> reason ?? "記入なし"}}
                     </div>
                 </td>
                 
                 <td>    
-                    <a href="#?id={{$request ->id}}" class="btn btn-primary">詳細</a>
-                    <a href="#" class="btn btn-primary">承認</a>
-                    <a href="#" class="btn btn-secondary">却下</a>
+                <a href="#?id={{$request ->id}}" class="btn btn-secondary">詳細</a>
+
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal1">
+                    承認
+                </button>
+
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#Modal2">
+                    却下
+                </button>
                 </td>
             </tr>
             @endforeach
@@ -52,3 +58,41 @@
         </table>
     </div>
 @endsection
+
+<div class="modal fade" id="Modal1" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="ModalLabel">確認</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        承認しますか？
+
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">キャンセル</button>
+        <button type="submit" class="btn btn-primary">承認</button>
+        </div>
+    </div>
+    </div>
+</div>
+
+<div class="modal fade" id="Modal2" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="ModalLabel">確認</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            却下しますか？
+
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">キャンセル</button>
+        <button type="submit" class="btn btn-primary">却下</button>
+        </div>
+    </div>
+    </div>
+</div>
