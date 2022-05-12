c1f0d2f5b5ff85f231da8e1d0d38cbe1c3715364
ppfkt <104489483+ppfkt@users.noreply.github.com>
2022-05-12 17:28:11 +0900

    Conflictの残骸を修正

--- resources/views/admin/user/admin_edit.blade.php
+++ resources/views/admin/user/admin_edit.blade.php
@@ -105,22 +105,13 @@
               <label for="InputName" class="form-label" >名前</label>
               <input type="text" class="form-control" value="{{$user['name']}}" id="InputName" name ='name' required>
             </div>
-<<<<<<< HEAD
             
-=======
-
->>>>>>> 10ec15a3131b86f718e0577512c03712aa8735f3
             <div class="mb-3">
                 <label for="InputMemo" class="form-label">社員メモ</label>
                 <input type="text" class="form-control" id="InputMemo" value="{{$user->user_memo->memo}}" name='memo' >
               </div>
-<<<<<<< HEAD
-            
+              
               <div class="mb-3">
-=======
-
-            <div class="mb-3">
->>>>>>> 10ec15a3131b86f718e0577512c03712aa8735f3
                 <label for="InputemployeeID" class="form-label">社員番号</label>
                 <input type="text" class="form-control" value="{{$user['employee_id']}}" name='employee_id' id="InputemployeeID" required>
               </div>
@@ -140,7 +131,6 @@
                 <option value="0">一般ユーザー</option>
                 <option value="1">管理者</option>
                 @endif
-<<<<<<< HEAD
               </select>
             </div>
 
@@ -148,15 +138,6 @@
               部署<select class="form-select" aria-label="部署" name='department' id="InputDepartment" required>
 
                 @foreach($departments as $item)
-=======
-                  </select>
-              </div>
-
-            <div class="mb-3">
-                部署<select class="form-select" aria-label="部署" name='department' id="InputDepartment" required>
-
-                    @foreach($departments as $item)
->>>>>>> 10ec15a3131b86f718e0577512c03712aa8735f3
                 @if($item['id'] == $user['department'])
                 <option selected value = "{{$item['id']}}">{{$item['name']}}</option>
                 @else
@@ -170,8 +151,7 @@
                 <label for="InputPassword" class="form-label">パスワード</label>
                 <input type="password" class="form-control" id="InputPassword" name='password' disabled>
               </div>
-<<<<<<< HEAD
-                
+
               <div class="mb-3">
                   <label for="InputEmail" class="form-label">メールアドレス</label>
                   <input type="email" class="form-control" id="InputEMail" name='email' value = "{{$user ['email']}}" required>
@@ -183,20 +163,6 @@
                 </div>
                 
                 <div class="mb-3">
-=======
-
-            <div class="mb-3">
-                  <label for="InputEmail" class="form-label">メールアドレス</label>
-                  <input type="email" class="form-control" id="InputEMail" name='email' value = "{{$user ['email']}}" required>
-              </div>
-
-            <div class="mb-3">
-                  <label for="InputHoliday" class="form-label">有給休暇</label>
-                  <input type="number" class="form-control" name='paid_holiday' id="InputHoliday" value = "{{$user ['paid_holiday']}}" required>
-                </div>
-
-            <div class="mb-3">
->>>>>>> 10ec15a3131b86f718e0577512c03712aa8735f3
                   <label for="InputJoined" class="form-label">入社日</label>
                   <input type="text" class="form-control" name='joined_date' id="InputJoined" placeholder ='YYYY-MM-DD' value = "{{$user ['joined_date']}}" required>
                 </div>
