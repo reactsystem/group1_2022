a7831b0dae194c0eac86fa66fdc0e8ef0deba652
kadokawa <kadokawa@ppfpart.co.jp>
2022-05-12 12:29:11 +0900

    各種情報編集画面のタイトルを修正

--- resources/views/admin/settings/general/edit.blade.php
+++ resources/views/admin/settings/general/edit.blade.php
@@ -21,7 +21,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <h2 class="fw-bold">各種情報確認</h2>
+                <h2 class="fw-bold">各種情報編集</h2>
             </div>
             <div class="col-md-6">
                 <button onclick="saveGeneralConfig()" id="saveBtn" class="btn btn-primary"
