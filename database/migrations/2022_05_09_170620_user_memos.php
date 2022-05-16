9707bce5002e8261021260e25a56b21672eb5975
PPF\yamaoka <yamaoka@ppfpart.co.jp>
2022-05-16 10:01:23 +0900

    migrationのmemosテーブルのmemosカラムをnullableに変更

--- database/migrations/2022_05_09_170620_user_memos.php
+++ database/migrations/2022_05_09_170620_user_memos.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('user_memos', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->string('memo');
+            $table->string('memo')->nullable();
             $table->timestamps();
         });
     }
