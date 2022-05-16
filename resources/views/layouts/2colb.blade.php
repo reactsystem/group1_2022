6a79e529d109438a960a53dd93a17d7f543b5dc8
kadokawa <kadokawa@ppfpart.co.jp>
2022-05-16 11:31:50 +0900

    最低限のレスポンシブ対応を実施

--- resources/views/layouts/2colb.blade.php
+++ resources/views/layouts/2colb.blade.php
@@ -2,10 +2,11 @@
 
 @section('basement')
     <div class="row" style="position: fixed; width: 100%; z-index: -100">
-        <div class="col-md-6 text-white" style="height: 100vh; background-color: #222;">
+        <div class="col-lg-6 col-md-12 text-white" style="height: 100vh; background-color: #222;">
 
         </div>
-        <div class="col-md-6 bg-light text-white" style="height: 100vh">
+        <div class="col-md-6 bg-light text-white d-xl-inline d-md-inline d-sm-none d-none d-lg-inline"
+             style="height: 100vh">
 
         </div>
     </div>
