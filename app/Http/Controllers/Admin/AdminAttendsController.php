804bb4d892bd79233fc7747616294648b515a44e
kadokawa <kadokawa@ppfpart.co.jp>
2022-05-17 16:33:41 +0900

    削除した部署が社員登録・編集画面に表示される問題を修正、管理通知の戻るボタン表記ミスを修正

--- app/Http/Controllers/Admin/AdminAttendsController.php
+++ app/Http/Controllers/Admin/AdminAttendsController.php
@@ -38,7 +38,7 @@ class AdminAttendsController extends Controller
 
     // ユーザー新規登録
     function admin_new(){
-        $departments = Department::all();
+        $departments = Department::where('deleted_at', null)->get();
         return view('admin/user/admin_new',['departments' => $departments]);
     }
 
@@ -60,24 +60,29 @@ class AdminAttendsController extends Controller
             return redirect('/admin/attends');
         }
 
-        $user = User::find($request -> id);
-        $departments = Department::all();
+        $user = User::find($request->id);
+        $departments = Department::where('deleted_at', null)->get();
         if(empty($user_memo))$user_memo = '';
         return view('admin/user/admin_edit',['user' => $user,'departments' => $departments,'user_memo' =>$user_memo]);
     }
 
     // ユーザー追加
-    function add_new_user(Request $request){
+    function add_new_user(Request $request)
+    {
+        $department = Department::find($request->department ?? 0);
+        if ($department == null || $department->deleted_at != null) {
+            return redirect('/admin/attends/new')->with('error', '指定された部署が存在しません。');
+        }
 
         $user = new User();
-        $user->name=$request->name;
+        $user->name = $request->name;
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
         $user->department = $request->department;
-        $user->employee_id=$request->employee_id;
-        $user->group_id=$request->group_id;
-        $user->joined_date=$request->joined_date;
-        $user->paid_holiday=$request->paid_holiday;
+        $user->employee_id = $request->employee_id;
+        $user->group_id = $request->group_id;
+        $user->joined_date = $request->joined_date;
+        $user->paid_holiday = $request->paid_holiday;
 
         $user->save();
         $last_insert_id = $user->id;
@@ -94,15 +99,55 @@ class AdminAttendsController extends Controller
     }
 
     // ユーザー更新
-    function update_user(Request $request){
-        $user =User::find($request -> id);
-        $user->name=$request->name;
-        $user->email=$request->email;
-        $user->department=$request->department;
-        $user->employee_id=$request->employee_id;
-        $user->group_id=$request->group_id;
-        $user->joined_date=$request->joined_date;
-        $user->left_date=$request->left_date;
+    function update_user(Request $request)
+    {
+        /*
+         *  保留
+         *
+         $rules = [
+            'name' => 'required',
+            'email' => 'required',
+            'department' => 'required|numeric',
+            'employee_id' => 'required|numeric',
+            'group_id' => 'required|numeric',
+            'paid_holiday' => 'required|numeric',
+        ];
+        $messages = [
+            'name.required' => '名前を入力してください。',
+            'email.numeric' => 'メールアドレスを入力してください',
+            'department.required' => '部署を選択してください',
+            'employee_id.required' => '社員番号を入力してください',
+            'group_id.required' => '権限を選択してください',
+            'paid_holiday.required' => '有給残日数を記入してください',
+            'department.numeric' => '部署を正しく選択してください',
+            'employee_id.numeric' => '社員番号を数字で入力してください',
+            'group_id.numeric' => '権限を数字で入力してください',
+            'paid_holiday.numeric' => '有給残日数を数字で入力してください',
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if ($validator->fails()) {
+            return redirect('/admin/attends/admin_edit')->with('error', '必須項目が記入されていません。');
+            //return response()->json(["error" => true, "code" => 1, "message" => "必須項目が記入されていません", "errors" => $validator->errors()]);
+        }
+         */
+
+        $department = Department::find($request->department ?? 0);
+        if ($department == null || $department->deleted_at != null) {
+            return redirect('/admin/attends/admin_edit')->with('error', '指定された部署が存在しません。');
+        }
+
+        $user = User::find($request->id);
+        if ($user == null) {
+            return redirect('/admin/attends/admin_edit')->with('error', '指定されたユーザーが存在しません。');
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->department = $request->department;
+        $user->employee_id = $request->employee_id;
+        $user->group_id = $request->group_id;
+        $user->joined_date = $request->joined_date;
+        $user->left_date = $request->left_date;
         $user->paid_holiday = $request->paid_holiday;
 
         $user->save();
